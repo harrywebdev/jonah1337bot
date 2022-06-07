580a14a912d30a4377e8790182908adfcf6cb7a8
Harry B <harry@gethornet.com>
2022-06-07 09:15:30 +0200

    add dev dumper and telegram bot check

--- app.php
+++ app.php
@@ -1,9 +1,19 @@
 <?php
 
+use Telegram\Bot\Api;
+
 require 'vendor/autoload.php';
 
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
+$telegram = new Api($_ENV['TELEGRAM_API_TOKEN']);
 
-$telegram = new \Telegram\Bot\Api($_ENV['TELEGRAM_API_TOKEN']);
+
+$response = $telegram->getMe();
+
+$botId = $response->getId();
+$firstName = $response->getFirstName();
+$username = $response->getUsername();
+
+dd($botId, $firstName, $username);
